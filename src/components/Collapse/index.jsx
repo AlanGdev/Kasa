49ad0304a92c384e007855315fb2b1257b69fd18
AlanGdev <alan.grolleau@gmail.com>
2025-01-06 16:16:22 +0100

    Ajout de Font Awesome Création du composant Collapse (+CSS) / Animation(mise en place)

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types"
+import { useState } from "react"
+import styles from './Collapse.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faChevronDown} from '@fortawesome/free-solid-svg-icons'
+
+function Collapse({title,content,size}){
+    const[isOpen,setIsOpen]=useState(false)
+    const collapseSize=size==='large'?'collapse':'collapse-medium'
+    const option=isOpen?'--open':''
+    console.log(option)
+    return(
+        <div className={styles[collapseSize]}>
+            <div className={styles[`${collapseSize}__top-bar`]}>
+                <p className={styles[`${collapseSize}__top-bar__title`]}>{title}</p>
+                <button className={`${styles[`${collapseSize}__top-bar__button`]} ${isOpen?styles[`${collapseSize}__top-bar__button${option}`]:''}`} onClick={()=>setIsOpen(!isOpen)}><FontAwesomeIcon icon={faChevronDown} /></button>
+            </div>
+            <div className={`${styles[`${collapseSize}__frame`]} ${isOpen?styles[`${collapseSize}__frame${option}`]:''}`}>
+                {Array.isArray(content)?
+                <ul className={styles[`${collapseSize}__frame__content`]}>
+                    {content.map((item)=>
+                    <li key={item}>{item}</li>)}
+                </ul>:
+                <p className={`${styles[`${collapseSize}__frame__content`]} ${isOpen?styles[`${collapseSize}__frame__content${option}`]:''}`}>{content}</p>}
+            </div>
+        </div>
+    )
+
+}
+Collapse.propTypes={
+    title: PropTypes.string.isRequired,
+    content: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.array
+    ]).isRequired,
+    size:PropTypes.string
+}
+export default Collapse
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,13 @@
 import picture from '../../assets/logoWhite.png'
 import styles from './Footer.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCopyright} from '@fortawesome/free-regular-svg-icons'
 function Footer(){
     return(
         <footer className={styles.footer}>
             <img className={styles.footer__img} src={picture}/>
             <p className={styles.footer__text}>
-                C 2020 Kasa. All rights reserved
+            <FontAwesomeIcon icon={faCopyright} /> 2020 Kasa. All rights reserved
             </p>
         </footer>
     )
