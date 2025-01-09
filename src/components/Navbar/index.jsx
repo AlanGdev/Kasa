ef241cb8bcd00a4b320439fc07831e454d1ab332
Alan.Grolleau <alan.grolleau@gmail.com>
2025-01-09 13:21:36 +0100

    Remplacement des routes statiques par une variable extraite du fichier routes.json sur la navbar et dans la page error

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,16 +1,19 @@
 import styles from './Navbar.module.scss'
 import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
+import routes from '../../routes.json'
 function Navbar(){
     const location=useLocation()
+    const routeAccueil=routes.find((route)=>route.element==='Home')
+    const routeApropos=routes.find((route)=>route.element==='Apropos')
     return(
         <nav className={styles.navbar}>
-            <Link to='/' className={`${styles.navbar__link} ${location.pathname==='/'?styles.navbar__linkUnderline:''} `}>
+            <Link to={routeAccueil.path} className={`${styles.navbar__link} ${location.pathname==='/'?styles.navbar__linkUnderline:''} `}>
                 Accueil
                 </Link>
             <Link
-             to='/Apropos' 
-             className={`${styles.navbar__link} ${location.pathname==='/Apropos'?styles.navbar__linkUnderline:''}`}>
+             to={routeApropos.path} 
+             className={`${styles.navbar__link} ${location.pathname==='/apropos'?styles.navbar__linkUnderline:''}`}>
                 A Propos
                 </Link>
             </nav>
